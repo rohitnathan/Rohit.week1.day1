c762e8c64335d65f81a6ec628f9ef7fec08c7a4a
rohitnathan <96671607+rohitnathan@users.noreply.github.com>
2021-12-26 12:11:38 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,29 +2,31 @@ package week1.day2;
 
 public class Calculator {
 
-	public int add (int num1, int num2) {
-	return num1+num2;
-	
-     }
-	public double sub (int num3, int num4) {
-		return num3-num4;
+	public int add(int num1, int num2) {
+		return num1 + num2;
 	}
-	public double mul (int num5, int num6) {
-		return num5*num6;
+
+	public double sub(double d, double e) {
+		return d - e;
 	}
-	public int divide (int num7, int num8) {
-		return num7/num8;
-	
+
+	public double mul(double d, double e) {
+		return d * e;
 	}
-		
-		public static void main(String[] args) {
+
+	public int divide(int num7, int num8) {
+		return num7 / num8;
+
+	}
+
+	public static void main(String[] args) {
 		Calculator calc = new Calculator();
-		int add = calc.add(30,40);
-		double sub = calc.sub(50,40);
-		double mul = calc.mul(2,4);
-		int divide = calc.divide(50,10);
-		
-		System.out.println(add);
+		int add = calc.add(30, 40);
+		double sub = calc.sub(50.55, 40.25);
+		double mul = calc.mul(2.5, 4.5);
+		int divide = calc.divide(50, 10);
+
+		System.out.println("Addition" + add);
 		System.out.println(sub);
 		System.out.println(mul);
 		System.out.println(divide);
